efc547b4c686b5d587c9a4068c3cd4059aea5ed1
zhaoyifan <1784848264@qq.com>
2023-12-13 16:44:47 +0800

    图标本地化

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import {onBeforeMount, ref} from 'vue'
+import setIntroduction from '@/utils/setIconfont';
 const theme = ref({
   token: {
     colorPrimary: '#00b96b'
   }
 })
+onBeforeMount(() => {
+  // 设置批量第三方 icon 图标
+  setIntroduction.cssCdn();
+  // 设置批量第三方 js
+  setIntroduction.jsCdn();
+});
 </script>
 <template>
   <a-config-provider :theme="theme">
