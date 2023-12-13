a0dd4e13a3dbd02c2a266c7e26d3bb29df7fa95b
zhaoyifan <1784848264@qq.com>
2023-12-13 16:44:03 +0800

    图标本地化

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/less/global.less'
-import './assets/aliIcon/iconfont.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import '@/assets/less/global.less';
 import '@arco-design/web-vue/dist/arco.css';
 
 import VueArcoUI from 'vue-arco-ui';
-import 'vue-arco-ui/css'
+import 'vue-arco-ui/css';
 
-const app = createApp(App)
+const app = createApp(App);
 // window.rafTimeout = rafTimeout // 挂载到window上，全局可用，无需引入
 
 app.use(router).use(VueArcoUI).mount('#app')
