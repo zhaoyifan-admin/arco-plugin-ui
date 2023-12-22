181a2b55c6ac61a79d7ad4cf6db9b94b015e11f1
zhaoyifan <1784848264@qq.com>
2023-12-22 11:31:14 +0800

    优化组件属性

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import {createApp} from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/assets/less/global.less';
-import '@arco-design/web-vue/dist/arco.css';
+import '@arco-themes/vue-eh-arco-plugin/index.less';
+// import '@arco-design/web-vue/dist/arco.css';
 
 import VueArcoUI from '../packages'
 // import VueArcoUI from 'vue-arco-ui';
-import 'vue-arco-ui/css';
+// import 'vue-arco-ui/css';
+import '../dist/style.css'
 
 const app = createApp(App);
 // window.rafTimeout = rafTimeout // 挂载到window上，全局可用，无需引入
